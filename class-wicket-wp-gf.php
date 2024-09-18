ee6872edcc6dce604ddb0c33325ea156c90cb61a
Coulter Peterson <29346967+coulterpeterson@users.noreply.github.com>
2024-09-18 16:32:26 -0400

    Adding 'Disable communication preferences' checkbox to GF wrapper element

--- class-wicket-wp-gf.php
+++ class-wicket-wp-gf.php
@@ -160,6 +160,7 @@ if ( ! class_exists( 'Wicket_Gf_Main' ) ) {
                 let orgss_settings_panes = document.querySelectorAll('.wicket_orgss_setting');
                 let wwidget_ai_settings = document.querySelectorAll('.wicket_widget_ai_setting');
                 let wwidget_org_profile_settings = document.querySelectorAll('.wicket_widget_org_profile_setting');
+                let wwidget_person_pref_settings = document.querySelectorAll('.wicket_widget_person_prefs_setting');
                 let gf_fields_wrapper = document.querySelector('#gform_fields');
                 let gf_edit_field_button = document.querySelector('.gfield-field-action.gfield-edit');
                 let wicket_global_settings_hide_label = document.querySelector('.wicket_global_custom_settings #hide_label');
@@ -201,6 +202,16 @@ if ( ! class_exists( 'Wicket_Gf_Main' ) ) {
                     } else {
                         for (let wwidget_org_profile_setting of wwidget_org_profile_settings) {
                             wwidget_org_profile_setting.style.display = "none";
+                        }
+                    }
+                    // Person pref widget
+                    if( selectedField.type == "wicket_widget_prefs" ) {
+                        for (let wwidget_person_pref_setting of wwidget_person_pref_settings) {
+                            wwidget_person_pref_setting.style.display = "block";
+                        }
+                    } else {
+                        for (let wwidget_person_pref_setting of wwidget_person_pref_settings) {
+                            wwidget_person_pref_setting.style.display = "none";
                         }
                     }
 
--- includes/class-gf-field-widget-prefs.php
+++ includes/class-gf-field-widget-prefs.php
@@ -29,6 +29,57 @@ if (class_exists('GF_Field')) {
       ];
     }
 
+    public static function custom_settings( $position, $form_id ) {
+      //create settings on position 25 (right after Field Label)
+      if ( $position == 25 ) { ?>
+        <?php ob_start(); ?>
+
+        <div class="wicket_widget_person_prefs_setting" 
+             style="display:none;margin-bottom: 10px;" 
+             x-data="wwidgetPersonPrefsData" 
+             x-on:gf-wwidget-person-prefs-field-settings.window="loadFieldSettings" >
+            <input 
+              @change="SetFieldProperty('wwidget_prefs_hide_comm', $el.checked)" x-bind:value="wwidget_prefs_hide_comm"
+              type="checkbox" id="wwidget_prefs_hide_comm" class="wwidget_prefs_hide_comm">
+					  <label for="wwidget_prefs_hide_comm" class="inline">Disable communication preferences?</label>
+        </div>
+
+        <?php echo ob_get_clean(); ?>
+
+        <?php
+      }
+    }
+
+    public static function editor_script(){
+      ?>
+      <script>
+      document.addEventListener('alpine:init', () => {
+          Alpine.data('wwidgetPersonPrefsData', () => ({
+            wwidget_prefs_hide_comm: false,
+
+          loadFieldSettings(event) {
+            let fieldData = event.detail;
+
+            if( Object.hasOwn(fieldData, 'wwidget_prefs_hide_comm') ) {
+              // Handle checkboxes slightly differently
+              this.wwidget_prefs_hide_comm = fieldData.wwidget_prefs_hide_comm ? true : false;
+            }
+          },
+        }))
+      });
+
+      // Catching GF event via jQuery (which it uses) and re-dispatching needed values for easier use
+      jQuery(document).on('gform_load_field_settings', (event, field, form) => {
+        let customEvent = new CustomEvent("gf-wwidget-person-prefs-field-settings", {
+          detail: rgar( field, 'wwidget_prefs_hide_comm' )
+        });
+        window.dispatchEvent(customEvent);
+      });
+    </script>
+
+    <?php
+    }
+
     // Render the field
     public function get_field_input($form, $value = '', $entry = null) {
       if ( $this->is_form_editor() ) {
@@ -37,12 +88,27 @@ if (class_exists('GF_Field')) {
 
       $id = (int) $this->id;
 
+      $hide_comm_prefs = false;
+
+      foreach( $form['fields'] as $field ) {
+        if( gettype( $field ) == 'object' ) {
+          if( get_class( $field ) == 'GFWicketFieldWidgetPrefs' ) {
+            if( $field->id == $id ) {
+              if( isset( $field->wwidget_prefs_hide_comm ) ) {
+                $hide_comm_prefs = (bool) $field->wwidget_prefs_hide_comm;
+              }
+            }
+          }
+        }
+      }
+
       if( component_exists('widget-prefs-person') ) {
         // Adding extra ob_start/clean since the component was jumping the gun for some reason
         ob_start();
 
         get_component( 'widget-prefs-person', [ 
-          'classes'                          => [],
+          'classes'                      => [],
+          'hide_comm_prefs'              => $hide_comm_prefs,
           'preferences_data_field_name'  => 'input_' . $id,
         ], true );
 
