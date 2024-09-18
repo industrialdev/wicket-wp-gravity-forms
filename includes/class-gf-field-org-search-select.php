9fe7f865436dd1eda5726477d0abfd080b023b1f
Coulter Peterson <29346967+coulterpeterson@users.noreply.github.com>
2024-09-18 12:17:24 -0400

    Minor tweak to ORGSS

--- includes/class-gf-field-org-search-select.php
+++ includes/class-gf-field-org-search-select.php
@@ -282,48 +282,49 @@ if (class_exists('GF_Field')) {
         }
       }
 
-      if( component_exists('org-search-select') ) {
-        ob_start();
-        ?>
-        <script>
-          // Wait for the next buttons to be present on screen
-          jQuery(document).ready(function() {
-            var existCondition = setInterval(function() {
-            if (jQuery('[id^=gform_next_button_]').length) {
-                clearInterval(existCondition);
-                findActiveNextButton();
-            }
-            }, 100); // check every 100ms
-          });
-
-          // On page load, find out active next button, make note of it, and hide it
-          function findActiveNextButton() {
-            let next_buttons = jQuery('[id^=gform_next_button_]');
-            let active_next_button = null;
-            next_buttons.each(function(index) {
-              if(jQuery(this).is(":visible")) {
-                active_next_button = jQuery(this);
-              }
-            });
-            if(!active_next_button) {
-              return;
-            }
-            window.activeNextButtonId = active_next_button.attr('id');
-
-            // Don't hide next button as this would currently apply to all pages - 
-            // Hide with a specific ID rule in Code Chest or stylesheet instead
-            //active_next_button.css('opacity', 0);
+      // Add the GF-specific ORGSS frontend scripts
+      ob_start();
+      ?>
+      <script>
+        // Wait for the next buttons to be present on screen
+        jQuery(document).ready(function() {
+          var existCondition = setInterval(function() {
+          if (jQuery('[id^=gform_next_button_]').length) {
+              clearInterval(existCondition);
+              findActiveNextButton();
           }
+          }, 100); // check every 100ms
+        });
 
-          // Listen for org selection (existing or just created) to then proceed to the next GF page, of a multi-step          
-          jQuery(window).on('orgss-selection-made', (event) => {
-            let nextButton = jQuery('#' + window.activeNextButtonId);
-            nextButton.trigger('click');
+        // On page load, find out active next button, make note of it, and hide it
+        function findActiveNextButton() {
+          let next_buttons = jQuery('[id^=gform_next_button_]');
+          let active_next_button = null;
+          next_buttons.each(function(index) {
+            if(jQuery(this).is(":visible")) {
+              active_next_button = jQuery(this);
+            }
           });
-        </script>
-        <?
-        $frontend_script = ob_get_clean();
+          if(!active_next_button) {
+            return;
+          }
+          window.activeNextButtonId = active_next_button.attr('id');
 
+          // Don't hide next button as this would currently apply to all pages - 
+          // Hide with a specific ID rule in Code Chest or stylesheet instead
+          //active_next_button.css('opacity', 0);
+        }
+
+        // Listen for org selection (existing or just created) to then proceed to the next GF page, of a multi-step          
+        jQuery(window).on('orgss-selection-made', (event) => {
+          let nextButton = jQuery('#' + window.activeNextButtonId);
+          nextButton.trigger('click');
+        });
+      </script>
+      <?
+      $frontend_script = ob_get_clean();
+
+      if( component_exists('org-search-select') ) {
         return '<div>' . 
         get_component( 'org-search-select', [ 
           'classes'                                       => [],
